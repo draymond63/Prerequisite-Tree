2a0f3357a1a88f12be828961ca771bbd77632a15
Daniel Raymond <dan@raymond.ch>
2023-02-13 09:58:46 +0000

    Improved view-count efficiency

--- server/api/topic.ts
+++ server/api/topic.ts
@@ -41,31 +41,37 @@ const getRelatedArticles = async (topic: string): Promise<string[]> => {
   return filterLinks(linkTitles);
 }
 
-// TODO: Filter results by views, number of links, etc.
+// TODO: Filter results by number of links
 // TODO: https://stackoverflow.com/questions/5323589/how-to-use-wikipedia-api-to-get-the-page-view-statistics-of-a-particular-page-in
 const filterLinks = async (articles: string[]): Promise<string[]> => {
   console.log(articles.length)
-  const viewCounts: Record<string, number> = {};
-  for (const article in articles) {
+  const resultPromises: Promise<WikiMediaResponse>[] = [];
+  for (const i in articles) {
     const path = [
       "en.wikipedia",
       "all-access", // or "desktop" or "mobile-app" or "mobile-web"
       "user", // or "spider" or "all-agents"
-      article,
+      encodeURIComponent(articles[i]),
       "monthly", // or "daily" or "hourly"
       "20230101",
       "20230201",
     ];
     const URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" + path.join("/");
-    const results = await $fetch(URL) as WikiMediaResponse; // ! Bad await call
-    if (results.items) {
-      const viewCount = results.items.reduce((acc, { views }) => acc + views, 0);
-      console.log(viewCount);
-      viewCounts[article] = viewCount;
-    }
+    resultPromises.push($fetch(URL).catch(r => ({})) as Promise<WikiMediaResponse>);
   }
-  console.log(viewCounts);
-  return articles;
+  const results = await Promise.all(resultPromises);
+  const viewCountEntries: [string, number][] = results.map((result) => {
+    if (result.items) {
+      const article = result.items[0].article;
+      const viewCount = result.items.reduce((acc, { views }) => acc + views, 0);
+      return [article, viewCount];
+    }
+    else {
+      return ["", 0];
+    }
+  });
+  // const viewCounts = Object.fromEntries(viewCountEntries);
+  return viewCountEntries.filter(([_, views]) => views > 5000).map(([article, _]) => article);
 }
 
 const getDescription = async (topic: string): Promise<string> => {
