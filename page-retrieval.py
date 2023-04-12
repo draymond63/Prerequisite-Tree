e5575b21cc73d9f9aafe6071ab8edade2f760a7a
Daniel Raymond <dan@raymond.ch>
2023-04-12 02:04:36 +0200

    Page text generator

--- page-retrieval.py
+++ page-retrieval.py
@@ -1,26 +1,47 @@
+import pandas as pd
 from xml.etree.cElementTree import Element, iterparse
 
-# TODO: Get new dataset with article text
-file_path = 'datasets/raw/enwikibooks-20230401-pages-meta-current.xml'
-root = iterparse(file_path, events=('start', 'end'))
+def get_pages(query: list):
+	file_path = 'datasets/raw/enwikibooks-20230401-pages-meta-current.xml'
+	root = iterparse(file_path)
+	page_active = False
+	current_title = None
+	current_id = None
+	current_text = None
+	calls_made = 0
+	element: Element
+	for event, element in root:
+		if 'page' in element.tag:
+			page_active = True
+		elif 'revision' in element.tag:
+			page_active = False
+			current_title = None
+			current_id = None
+			current_text = None
+		elif 'title' in element.tag:
+			current_title = element.text
+		elif 'id' in element.tag:
+			current_id = element.text
+		elif 'text' in element.tag:
+			current_text = element.text
+			if page_active and current_title in query:
+				if 'REDIRECT' in current_text:
+					print(f'WARNING: {current_title} is a redirect')
+				calls_made += 1
+				yield current_title, current_id, current_text
+		if calls_made >= len(query):
+			break
 
-path = []
-element: Element
-for event, element in root:
-	if event == 'start':
-		path.append(element.tag)
-	elif event == 'end':
-		# if 'page' in element.tag:
-		# 	title = element.find('{http://www.mediawiki.org/xml/export-0.10/}title')
-		# 	element_id = element.find('{http://www.mediawiki.org/xml/export-0.10/}id')
-		# 	print(title.text)
-		# 	for child in element:
-		# 		print(child.tag)
-		# if 'title' in element.tag:
-		# 	print(element.text)
-		print('\n', element.tag)
-		for child in element:
-			print('C:', child.tag)
-			for c2 in child:
-				print('\t', c2.tag)
-		path.pop()
+def parse_headlines(text: str) -> list:
+	print(text)
+	headlines = []
+	for line in text.split('\n'):
+		if line.startswith('='):
+			header = line.strip().strip('=').strip()
+			headlines.append(header)
+	return headlines
+
+if __name__ == '__main__':
+	pages = pd.read_csv('datasets/departments.tsv', sep='\t', header=None).values.flatten().tolist()
+	for title, id, text in get_pages(pages):
+		print(parse_headlines(text))
