42983888a66967b4c9f8cbd1c9562f97ef344139
Daniel Raymond <dan@raymond.ch>
2023-04-16 22:23:11 +0000

    Converted page retrieval to a class

--- page-retrieval.py
+++ page-retrieval.py
@@ -1,35 +1,43 @@
 import pandas as pd
 from xml.etree.cElementTree import Element, iterparse
 from tqdm import tqdm
-from typing import Optional, Iterable
+from typing import Optional, List
 
-def get_pages(query: Optional[Iterable[str]] = None):
-	file_path = 'datasets/raw/enwikibooks-20230401-pages-meta-current.xml'
-	root = iterparse(file_path)
-	page_active, page_title, page_id, page_text = False, '', '', ''
-	calls_made = 0
-	element: Element
-	for event, element in root:
-		if 'page' in element.tag:
-			page_active = True
-		elif 'revision' in element.tag:
-			page_active, page_title, page_id, page_text = False, '', '', ''
-		elif 'title' in element.tag:
-			page_title = element.text
-		elif 'id' in element.tag:
-			page_id = element.text
-		elif 'text' in element.tag:
-			page_text = element.text
-			valid_title = query is None or page_title in query
-			if page_active and valid_title:
-				# if 'REDIRECT' in page_text:
-				# 	print(f'WARNING: {page_title} is a redirect')
-				# else:
-				# 	print(page_title)
-				calls_made += 1
-				yield page_title, page_id, page_text
-		if query is not None and calls_made >= len(query):
-			break
+
+class PageRetriever:
+	def __init__(self, file_path='datasets/raw/enwikibooks-20230401-pages-meta-current.xml', show_warnings=False):
+		self.file_path = file_path
+		self.show_warnings = show_warnings
+	
+	# TODO: Add progress bar
+	def get(self, page_titles: Optional[List[str]] = None, pbar=False):
+		root = iterparse(self.file_path)
+		page_count = 0
+		page_active, page_title, page_id, page_text = False, '', '', ''
+		for event, element in root:
+			if 'page' in element.tag:
+				page_active = True
+			elif 'revision' in element.tag:
+				page_active, page_title, page_id, page_text = False, '', '', ''
+			elif 'title' in element.tag:
+				page_title = element.text
+			elif 'id' in element.tag:
+				page_id = element.text
+			elif 'text' in element.tag:
+				page_text = element.text
+				if page_active and self.is_valid_title(page_title, page_titles):
+					page_count += 1
+					yield page_title, page_id, page_text
+			if page_titles is not None and page_count >= len(page_titles):
+				break
+
+	def is_valid_title(self, page_title, pages: Optional[List[str]] = None):
+		if pages is None:
+			return True
+		if self.show_warnings and 'REDIRECT' in page_title:
+			print(f'WARNING: {page_title} is a redirect')
+		page_urend = page_title.replace(' ', '_') # Works for all but 11 (useless) article titles
+		return page_urend in pages
 
 def parse_headlines(text: str) -> list:
 	print(text)
@@ -41,7 +49,7 @@ def parse_headlines(text: str) -> list:
 	return headlines
 
 if __name__ == '__main__':
-	pages = pd.read_csv('datasets/articles.tsv', sep='\t').values.flatten().tolist()
+	pages = pd.read_csv('datasets/books.tsv', sep='\t').values.flatten().tolist()
 	# pages = ['Shelf:Systems_engineering', 'Shelf:Systems_engineering/all_books', 'Systems_engineering']
-	for title, page_id, text in tqdm(get_pages(pages), total=len(pages)):
-		print(title, parse_headlines(text))
+	for title, page_id, text in PageRetriever().get(pages):
+		print(title, text)
