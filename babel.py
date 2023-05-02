e1b0cff5e1f3bc2cac45c5b40df5bb58ff75b803
Daniel Raymond <dan@raymond.ch>
2023-05-02 14:18:44 -0400

    Started babel prereq extraction

--- babel.py
+++ babel.py
@@ -1,54 +1,41 @@
 
 
+import spacy
 import babelnet as bn
+
+from spacy import displacy
+from spacy.tokens.doc import Doc
 from babelnet import Language, BabelSynset
 from babelnet.resources import BabelSynsetID
 from joblib import Memory
+from typing import Dict, Set
 
 memory = Memory("datasets/cache")
+model = spacy.load('en_core_web_sm')
 
 @memory.cache
-def get_synset(word: str = 'bn:03566112n') -> BabelSynset:
+def get_synset(babel_id: str = 'bn:03566112n') -> BabelSynset:
     print('RUNNING FUNCTION')
-    return bn.get_synset(BabelSynsetID('bn:03566112n'))
+    return bn.get_synset(BabelSynsetID(babel_id))
 
+def get_prerequisites(synset: BabelSynset) -> Dict[str, Set[BabelSynset]]:
+    # TODO: Extract main phrase of sentence
+    # TODO: For each noun chunk, find all possible synsets
+    # TODO: Determine best synset for each noun chunk based on categorical similarity to original synset
+    for gloss in synset.glosses():
+        parsed = model(gloss.gloss)
+        for noun in parsed.noun_chunks:
+            print(f'"{noun.text}"', noun.root.dep_, noun.root.head.text)
 
-by = get_synset()
-# bs = by.main_sense(Language.EN)
-# print('\n'.join([str(((g.gloss, g.token_ids))) for g in by.glosses()]))
-# print(by.categories(Language.EN))
-# print(by.domains)
-print(by.senses_by_word('systems', Language.EN))
+def get_referenced_synsets(synset: BabelSynset) -> Set[BabelSynsetID]:
+    referenced_synsets = set()
+    for gloss in synset.glosses():
+        for token in gloss.token_ids:
+            referenced_synsets.add(token.id)
+    return referenced_synsets
 
-
-# import concurrent.futures
-# from datetime import datetime
-# from sys import stdout
-
-# # function called from the threads
-# def func(name: str, word: str):
-#     stdout.write(datetime.now().strftime("%H:%M:%S.%f") + " - Start - " + name + "\n")
-#     synsets = bn.get_synsets(word, from_langs=[Language.EN])
-#     glosses = []
-#     for synset in synsets:
-#         gloss = synset.main_gloss(Language.EN)
-#         if gloss:
-#             glosses.append(gloss.gloss)
-#     stdout.write(datetime.now().strftime("%H:%M:%S.%f") + " - End   - " + name + "\n")
-#     return {word: glosses}
-
-
-# word_list = ["vocabulary", "article", "time", "bakery", "phoenix", "stunning", "judge", "clause", "anaconda",
-#              "patience", "risk", "scribble", "writing", "zebra", "trade"]
-
-# with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-#     future = []
-#     for i, w in enumerate(word_list):
-#         future.append(executor.submit(func, f'Thread {i} "{w}"', w))
-#     results = {}
-#     for f in future:
-#         results.update(f.result())
-
-# for w, gs in results.items():
-#     for g in gs:
-#         print(w, g, sep='\t')
+if __name__ == '__main__':
+    by = get_synset('bn:03888638n')
+    print(get_referenced_synsets(by))
+    # get_prerequisites(by)
+    # print('\n'.join([str(((g.gloss, g.token_ids))) for g in by.glosses()]))
