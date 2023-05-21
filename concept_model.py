f0697e1d8243bc4e8fa9cfcba930c57a6a01c330
Daniel Raymond <dan@raymond.ch>
2023-05-20 19:46:11 -0700

    Initial topic set generation

--- concept_model.py
+++ concept_model.py
@@ -53,49 +53,53 @@ class PrerequisiteMap:
 		return self.get_concept(synset)
 	
 	@staticmethod
-	def _is_named_entity(synset) -> bool:
+	def _is_invalid_concept(synset: BabelSynset) -> bool:
+		# TODO: Ensure sysnet is_key_concept
 		return synset.type != 'CONCEPT'
 
 	def find_synset_wiki(self, name: str, wiki_category = None) -> BabelSynset:
 		if wiki_category is None:
 			return bn.get_synset(WikipediaID(name, language=self.lang)) # TODO: Name is not an ID
-		synsets = bn.get_synsets(name, from_langs={self.lang}, synset_filters={self._is_named_entity})
+		synsets = bn.get_synsets(name, from_langs={self.lang}, synset_filters={self._is_invalid_concept})
 		if len(synsets) == 0:
-			raise Exception(f'No synsets found for {name}')
-		print(synsets)
+			raise LookupError(f'No synsets found for {name}')
 		distances = [self.distance_to_category(synset, wiki_category) for synset in synsets]
-		print(distances)
 		distances = {i: dist for i, dist in enumerate(distances) if dist is not None}
 		if len(distances) == 0:
-			raise Exception(f'No synsets found like {name} in category {wiki_category}')
-		print(distances)
+			raise LookupError(f'No synsets found like {name} in category {wiki_category}')
 		best_index = min(distances, key=distances.get)
-		print(best_index)
 		return synsets[best_index]
 
 	@memory.cache
-	def find_synset(self, babel_id: BabelSynsetID) -> BabelSynset:
-		return bn.get_synset(babel_id)
+	def get_synset(self, babel_id: BabelSynsetID) -> BabelSynset:
+		return bn.get_synset(babel_id, to_langs={self.lang})
 
 	def get_concept(self, synset: BabelSynset) -> 'Concept':
 		name = synset.main_sense().lemma # TODO: Wrong
 		if synset.id in self.map:
 			return self.map[synset.id]
-		definitions = [Definition(gloss.gloss, self._generate_prereqs(gloss.gloss)) for gloss in synset.glosses()]
+		category = synset.categories[0].name # TODO: Better principle category selection
+		definitions = [Definition(gloss.gloss, self._generate_prereqs(gloss.gloss, category)) for gloss in synset.glosses()]
 		topic_set = self._generate_topic_set(synset)
 		concept = Concept(name, topic_set, definitions)
 		self.map[concept.wikiID] = concept
 		return concept
 
-	def _generate_prereqs(self, definition: str) -> 'Set[BabelSynsetID]':
+	def _generate_prereqs(self, definition: str, wiki_category: Optional[str] = None) -> Set[BabelSynsetID]:
 		# TODO: Extract main phrase of sentence
 		# TODO: For each noun chunk, find all possible synsets
 		# TODO: Determine best synset for each noun chunk based on categorical similarity to original synset
 		parsed = self.model(definition)
+		prereqs = set()
 		for noun in parsed.noun_chunks:
 			cleaned_noun = self.clean_noun(noun.text)
-			# TODO: Get the synset for the noun
-			print(f'"{cleaned_noun}"', noun.root.dep_, noun.root.head.text)
+			try:
+				synset_candidate = self.find_synset_wiki(cleaned_noun, wiki_category)
+			except LookupError:
+				synset_candidate = self.find_find_synset_wiki(cleaned_noun)
+			# TODO: Wasteful to just use the id
+			prereqs.add(synset_candidate.id)
+		return prereqs
 
 	# TODO: Find spacy way of dropping stop words
 	def clean_noun(self, text: str) -> str:
@@ -103,13 +107,12 @@ class PrerequisiteMap:
 		words = [word for word in words if word not in self.model.Defaults.stop_words]
 		return ' '.join(words)
 
-	def _generate_topic_set(self, synset: BabelSynset) -> 'Set[BabelSynsetID]':
-		# TODO: Switch to WIKI only relations?
-		relations = synset.outgoing_edges([
-			BabelPointer.ANY_HOLONYM,
-			BabelPointer.ANY_HYPONYM,
-			BabelPointer.TOPIC,
-		])
+	@staticmethod
+	def _generate_topic_set(synset: BabelSynset) -> 'Set[BabelSynsetID]':
+		# TODO: Switch to WIKI only relations? Are these the correct relations?
+		relations = synset.outgoing_edges(BabelPointer.ANY_HOLONYM)
+		relations += synset.outgoing_edges(BabelPointer.ANY_HYPONYM)
+		relations += synset.outgoing_edges(BabelPointer.TOPIC)
 		return {relation.id_target for relation in relations}
 
 	def parent_category_path(self, child: str, parent: str) -> Optional[List[str]]:
@@ -142,5 +145,7 @@ class PrerequisiteMap:
 
 
 if __name__ == '__main__':
-	# BabelSynsetID('bn:03566112n')
-	print(PrerequisiteMap().find_concept('Control Theory', 'Mathematics'))
+	map = PrerequisiteMap()
+	synset = map.get_synset(BabelSynsetID('bn:03566112n'))
+	print(synset)
+	print(map.get_concept(synset))
