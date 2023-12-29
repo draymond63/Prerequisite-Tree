fc997bc698aa949cc4a6248b96ad277443ce24cb
Daniel Raymond <dan@raymond.ch>
2023-12-29 16:03:05 -0500

    add column specifier

--- sqldump_to_csv.py
+++ sqldump_to_csv.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-# * COPIED FROM https://github.com/jamesmishra/mysqldump-to-csv
+# * ADAPTED FROM https://github.com/jamesmishra/mysqldump-to-csv
 # * RUN AS `python sqldump_to_csv.py dump.sql > dump.csv`
 
 import fileinput
 import csv
 import sys
 from tqdm import tqdm
+from typing import List, Optional
+
+
+__all__ = ['sqldump_to_csv']
 
 
 def is_insert(line: str):
@@ -33,10 +37,21 @@ def values_sanity_check(values):
     return True
 
 
-def parse_values(values, outfile):
+def parse_values(values: str, outfile, columns: Optional[List[int]]=None):
     """
     Given a file handle and the raw values from a MySQL INSERT
     statement, write the equivalent CSV to the file
+
+    Parameters
+    ----------
+    values : str
+        Raw values from a MySQL INSERT statement. May contain multiple CSV lines
+    outfile : file handle
+        A file handle to write the CSV to
+    columns : list, optional
+        Which columns to keep in the CSV. If not specified, all columns are kept
+    row_filter : callable, optional
+        A function with signature f(row[columns]) -> bool that returns whether to keep a row
     """
     latest_row = []
 
@@ -74,6 +89,8 @@ def parse_values(values, outfile):
                 # If we've found a new row, write it out
                 # and begin our new one
                 if new_row:
+                    if columns is not None:
+                        latest_row = [latest_row[i] for i in columns]
                     writer.writerow(latest_row)
                     latest_row = []
                 # If we're beginning a new row, eliminate the
@@ -88,26 +105,33 @@ def parse_values(values, outfile):
         # the close paren.
         if latest_row[-1][-2:] == ");":
             latest_row[-1] = latest_row[-1][:-2]
+            if columns is not None:
+                latest_row = [latest_row[i] for i in columns]
             writer.writerow(latest_row)
+
+
+def sqldump_to_csv(source_file, outfile, columns: Optional[List[int]]=None):
+    try:
+        for line in tqdm(source_file):
+            # Look for an INSERT statement and parse it.
+            if is_insert(line):
+                values = get_values(line)
+                if values_sanity_check(values):
+                    parse_values(values, outfile, columns)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 
 def main():
     """
     Parse arguments and start the program
     """
-    # Iterate over all lines in all files
-    # listed in sys.argv[1:]
-    # or stdin if no args given.
-    try:
-        # openhook=fileinput.hook_encoded("utf-8")
-        for line in tqdm(fileinput.input(openhook=fileinput.hook_encoded("iso-8859-1"))):
-            # Look for an INSERT statement and parse it.
-            if is_insert(line):
-                values = get_values(line)
-                if values_sanity_check(values):
-                    parse_values(values, sys.stdout)
-    except KeyboardInterrupt:
-        sys.exit(0)
+    sqldump_to_csv(fileinput.input(openhook=fileinput.hook_encoded("iso-8859-1")), sys.stdout, columns=[0, 1, 6])
+
 
 if __name__ == "__main__":
     main()
+
+    # with open('datasets/raw/enwiki-latest-categorylinks.sql', 'r', encoding='iso-8859-1') as f: 
+    #     with open('datasets\raw\enwiki-latest-categorylinks.csv', 'w', encoding='utf-8') as outfile:
+    #         sqldump_to_csv(f, outfile, columns=[0, 1, 6])
